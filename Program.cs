b898127862ef8aaf5068b1223e33ac0d8888cc07
Silvia-Du <silviadu@hotmail.it>
2022-09-16 14:42:44 +0200

    Fine snack due(base senza controlli)

--- Program.cs
+++ Program.cs
@@ -1,37 +1,62 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 
-Console.WriteLine("Hello, World!");
-
-//l'utente inserisce due numeri in successione, il software stama il maggiore
+//1- l'utente inserisce due numeri in successione, il software stama il maggiore
 
 //int limit = 2;
 //int[] numbers = new int[2];
-int num1;
-int num2;
+//int num1;
+//int num2;
 
 
-    Console.WriteLine("inserisci il primo numero");
-    int userNumber1 = Convert.ToInt32(Console.ReadLine());
-    num1 = userNumber1;
+//    Console.WriteLine("inserisci il primo numero");
+//    int userNumber1 = Convert.ToInt32(Console.ReadLine());
+//    num1 = userNumber1;
 
-    Console.WriteLine("inserisci il secondo numero");
-    int userNumber2 = Convert.ToInt32(Console.ReadLine());
-    num2 = userNumber2;
+//    Console.WriteLine("inserisci il secondo numero");
+//    int userNumber2 = Convert.ToInt32(Console.ReadLine());
+//    num2 = userNumber2;
 
-if (num1 > num2)
+//if (num1 > num2)
+//{
+//    Console.WriteLine($"il primo numero({num1}) è maggiore");
+
+//}else if(num2 > num1)
+//{
+//    Console.WriteLine($"il secondo numero ({num2}) è maggiore");
+//}
+//else
+//{
+//    Console.WriteLine("i due numeri sono uguali");
+
+//}
+
+//2-L'utente inserisce due parole in successione, il software scrive prima quella piu corta e poi quella piu lunga
+
+string[] words = new string[2];
+Console.WriteLine($"ti chiederò di inserire due parole");
+
+for (int i=0; i< words.Length; i++)
 {
-    Console.WriteLine($"il primo numero({num1}) è maggiore");
+    Console.WriteLine($"scrivi la {i+1}' parola");
+    words[i]= Console.ReadLine();
 
-}else if(num2 > num1)
+}
+
+if(words[0].Length > words[1].Length)
 {
-    Console.WriteLine($"il secondo numero ({num2}) è maggiore");
+    Console.WriteLine($"la parola piu corta: {words[1]}");
+    Console.WriteLine($"la parola piu lunga: {words[0]}");
+}else if(words[1].Length > words[0].Length)
+{
+    Console.WriteLine($"la parola piu corta: {words[0]}");
+    Console.WriteLine($"la parola piu lunga: {words[1]}");
 }
 else
 {
-    Console.WriteLine("i due numeri sono uguali");
-
+    Console.WriteLine($"le due parole sono lunghe uguali");
 }
+
 
 
 
