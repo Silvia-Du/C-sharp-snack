694c1e250cc2fbc0eda8dc41831a50805f21afa9
Silvia-Du <silviadu@hotmail.it>
2022-09-16 15:01:07 +0200

    Fine sanck 4

--- Program.cs
+++ Program.cs
@@ -59,20 +59,38 @@ using System;
 
 //3- Chiedre 10 volte all'utente di inserire un numero, il software stampa la somma dei 10 numeri
 
-Console.WriteLine($"Ti chiederò di inserire dieci numeri");
-int total = 0;
+//Console.WriteLine($"Ti chiederò di inserire dieci numeri");
+//int total = 0;
 
-for (int i = 0; i < 10; i++)
+//for (int i = 0; i < 10; i++)
+//{
+//    Console.WriteLine($"inserisci il {i+1}' numero");
+//    int userNum = Convert.ToInt32(Console.ReadLine());
+//    total =  userNum + total;
+
+//}
+
+//Console.WriteLine($"La somma dei tuoi numeri è {total}");
+
+//4- calcola la somma e la media dei numeri da 2 a 10
+
+int[] numbers = { 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+int sum = default;
+int numAverage = default;
+
+for (int i = 0; i < numbers.Length; i++)
 {
-    Console.WriteLine($"inserisci il {i+1}' numero");
-    int userNum = Convert.ToInt32(Console.ReadLine());
-    total =  userNum + total;
+    sum = numbers[i] + sum;
 
 }
 
-Console.WriteLine($"La somma dei tuoi numeri è {total}");
+numAverage = sum / numbers.Length;
 
-//4- calcola la somma e la media dei numeri da 2 a 10
+Console.WriteLine($"la somma dei numeri da 2 a 10 è {sum}");
+Console.WriteLine($"la media dei numeri da 2 a 10 è {numAverage}");
+
+
+
 
 
 
